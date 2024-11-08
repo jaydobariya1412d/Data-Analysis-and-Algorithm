67e631f42780a84a04c4ef7e370a6332604033be
jaydobariya1412d <126901516+jaydobariya1412d@users.noreply.github.com>
2024-11-08 15:48:38 +0530

    Depth-First-Search (Binary Tree)

---
+++ Leetcode/PostOrderTraversal.java
@@ -0,0 +1,22 @@
+
+class PostOrderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        Deque<TreeNode> stk = new ArrayDeque<>();
+        stk.push(root);
+        while (!stk.isEmpty()) {
+            TreeNode node = stk.pop();
+            ans.addFirst(node.val);
+            if (node.left != null) {
+                stk.push(node.left);
+            }
+            if (node.right != null) {
+                stk.push(node.right);
+            }
+        }
+        return ans;
+    }
+}
